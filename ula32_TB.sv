0b6c358af33a0aa58d7386d537c9c85a92ced375
adrieleramoso <32364445+adrieleramoso@users.noreply.github.com>
2017-10-03 19:05:45 -0300

    Update ula32_TB.sv

--- ula32_TB.sv
+++ ula32_TB.sv
@@ -1,6 +1,12 @@
 +`timescale 1ns/1ns
 +
 +module ULA32_TB;
+  
++   logic clk;
++   logic [101:0] TestVector[10000:0];
++   logic [127:0] vectornum, errors;
++   logic [31:0] ExpectedResult;
++   logic [3:0] ExpectedFlags;
 +
 +//variáveis de entrada
 +   logic [31:0] a;
@@ -10,29 +16,44 @@
 +//variáveis de saída
 +   logic [31:0] Result;
 +   logic [3:0] ALUflags;
-+   logic neg;
-+   logic carry;
-+   logic zero;
-+   logic overflow;
-+
+
++// instanciar devide under test - dut
 +   ULA32 dut(
 +	.a(a),
 +	.b(b),
 +	.ALUcontrol(ALUcontrol),
 +	.Result(Result),
-+	.ALUflags(ALUflags),
-+	.neg(neg),
-+	.carry(carry),
-+	.zero(zero),
-+	.overflow(overflow)
++	.ALUflags(ALUflags)
 +   );
 +
-+   always
-+	begin
-+		
++//mostrar no transcript
+    initial begin
+ 	$display("\t\ta,\tb,\tALUcontrol,\tResult,\tALUflags");
+ 	$monitor("%b,\t%b,\t%b,\t%b, \t%b", a, b, ALUcontrol, Result, ALUflags); 
 +
++   end
+  
+  
++//clock
++   always begin 
++	clk = 1; #5; clk = 0; #5;
+    end
+  
++//leitura de vetores do arquivo texto; valores iniciais
++   initial begin 
++	$readmemb("TBVectors", TestVector);
++	vectornum = 0; errors = 0;
++   end
 +
-+	end
++//subida do clock
++   always @(posedge clk) begin
++	{a, b, ALUcontrol, ExpectedResult, ExpectedFlags} = TestVector[vectornum]; #10;
++   end
 +
++//descida do clock
++   always @(negedge clk) begin
++	$display("Saídas erradas para as entradas informadas %b,ou %b!=%b e/ou %b!=%b", {a, b, ALUcontrol}, ExpectedResult, Result, ExpectedFlags, ALUFlags);
++   	errors = errors +1;
++   end
 +
 +endmodule 
